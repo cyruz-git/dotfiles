718c520761a9f950e1d825cff15cc5e1bce6f947
Ciro Principe <focabresm@gmail.com>
2015-08-17 22:00:16 +0200

    Removed useless formatting

--- .colors/solarized/git-update-all.sh
+++ .colors/solarized/git-update-all.sh
@@ -16,22 +16,21 @@ rm -rf ./dircolors-solarized/.git
 if [[ -d ./tmux-colors-solarized ]]; then
 	rm -rf ./tmux-colors-solarized
 fi
-	git clone https://github.com/seebi/tmux-colors-solarized.git
-	rm -rf ./tmux-colors-solarized/.git
+git clone https://github.com/seebi/tmux-colors-solarized.git
+rm -rf ./tmux-colors-solarized/.git
 
 if [[ -d ./tty-solarized ]]; then
 	rm -rf ./tty-solarized
 fi
-	git clone https://github.com/joepvd/tty-solarized.git
-	rm -rf ./tty-solarized/.git
-	patch -p1 ./tty-solarized/tty-solarized-dark.sh ./patch/tty-solarized-dark.patch
-	patch -p1 ./tty-solarized/tty-solarized-light.sh ./patch/tty-solarized-light.patch
+git clone https://github.com/joepvd/tty-solarized.git
+rm -rf ./tty-solarized/.git
+patch -p1 ./tty-solarized/tty-solarized-dark.sh ./patch/tty-solarized-dark.patch
+patch -p1 ./tty-solarized/tty-solarized-light.sh ./patch/tty-solarized-light.patch
 
 if [[ -d ./vim-colors-solarized ]]; then
 	rm -rf ./vim-colors-solarized
 fi
-	git clone https://github.com/altercation/vim-colors-solarized.git
-	rm -rf ./vim-colors-solarized/.git
+git clone https://github.com/altercation/vim-colors-solarized.git
+rm -rf ./vim-colors-solarized/.git
 
 cd $PWD
-
